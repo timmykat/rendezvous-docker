868b1ff3a98b6fba40a3becde1e3de3207b35c05
Tim Kinnel <tkinnel@gmail.com>
2016-01-16 17:01:05 -0500

    Only TK email on staging, add restart to deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -38,6 +38,15 @@ set :keep_releases, 3
 
 namespace :deploy do
 
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+
+  after :publishing, :restart
+
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
       # Here we can do anything such as:
