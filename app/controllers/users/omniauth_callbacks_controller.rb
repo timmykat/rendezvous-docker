6dda7ee50481a49d398bda51f1dd604f1965b354
Tim Kinnel <tkinnel@gmail.com>
2016-03-19 16:46:14 -0400

    Comment out omniauth callbacks controller

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,16 +1,16 @@
-class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-    
-  def facebook
-    @user = User.from_omniauth(env["omniauth.auth"])
-    sign_in_and_redirect @user, event: :authentication
-      set_flash_message(:notice, :success, kind: "#{provider}".capitalize) if is_navigational_format?
-    else
-      session["devise.#{provider}_data"] = env["omniauth.auth"]
-      redirect_to sign_up_or_in_url
-    end
-  end
-
-  def failure
-    redirect_to root_path
-  end
-end
+# class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+#     
+# #   def facebook
+# #     @user = User.from_omniauth(env["omniauth.auth"])
+# #     sign_in_and_redirect @user, event: :authentication
+# #       set_flash_message(:notice, :success, kind: "#{provider}".capitalize) if is_navigational_format?
+# #     else
+# #       session["devise.#{provider}_data"] = env["omniauth.auth"]
+# #       redirect_to sign_up_or_in_url
+# #     end
+# #   end
+# # 
+# #   def failure
+# #     redirect_to root_path
+# #   end
+# end
