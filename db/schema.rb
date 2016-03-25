239ccf236188ad50f52b07035986d655b6ec2dac
Tim Kinnel <tkinnel@gmail.com>
2016-03-25 19:08:07 -0400

    Recover delayed_job

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160323182625) do
+ActiveRecord::Schema.define(version: 20160325230626) do
 
   create_table "attendees", force: :cascade do |t|
     t.string   "name",                       limit: 255
