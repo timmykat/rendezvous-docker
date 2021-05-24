62efa83fee2a78c343f010bc56fc13bbaa8be266
timmykat <tkinnel+github@gmail.com>
2021-05-24 05:53:49 -0400

    Add dkim support

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ lock '3.12.1'
 
 set :application, 'rendezvous'
 set :deploy_via, :copy
-set :rbenv_ruby, '2.7.0' 
+set :rbenv_ruby, '2.7.0'
 
 # set :repo_url, 'git@example.com:me/my_repo.git'
 
@@ -26,7 +26,7 @@ ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set :linked_files, fetch(:linked_files, []).push('dkim.pem', 'config/database.yml', 'config/secrets.yml')
 
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/uploads', 'public/registrations')
