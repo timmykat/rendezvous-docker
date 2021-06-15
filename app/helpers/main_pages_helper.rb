b244687740ec4c591bc212905ff55731ae7d7dd6
timmykat <tkinnel+github@gmail.com>
2021-06-14 21:13:16 -0400

    Create registration permission helper

--- app/helpers/main_pages_helper.rb
+++ app/helpers/main_pages_helper.rb
@@ -1,2 +1,5 @@
 module MainPagesHelper
+  def can_register?
+    (current_user && (current_user.has_role? :admin)) || (Time.now > Rails.configuration.rendezvous[:registration_window][:open] && Time.now <= Rails.configuration.rendezvous[:registration_window][:close])
+  end
 end
