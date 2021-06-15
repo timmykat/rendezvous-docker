6e7bb62190754b42be0efe574890190c687010b6
timmykat <tkinnel+github@gmail.com>
2021-06-14 20:58:46 -0400

    Read dkim from correct placce

--- config/initializers/dkim.rb
+++ config/initializers/dkim.rb
@@ -1,7 +1,7 @@
 if Rails.env.production?
   Dkim::domain      = 'citroenrendezvous.org'
   Dkim::selector    = 'dkim'
-  pem_file = File.expand_path(File.join(File.dirname(__FILE__), '..', 'dkim.pem'))
+  pem_file = File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'dkim.pem'))
   Dkim::private_key = open(pem_file).read
 
   # This will sign all ActionMailer deliveries
