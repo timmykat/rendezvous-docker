e3bfe5b1c1ed9725173cccba4dacf2b10dd54640
timmykat <tkinnel+github@gmail.com>
2021-06-14 20:52:36 -0400

    Typo in dkim initializer file

--- config/initializers/dkim.rb
+++ config/initializers/dkim.rb
@@ -2,7 +2,7 @@ if Rails.env.production?
   Dkim::domain      = 'citroenrendezvous.org'
   Dkim::selector    = 'dkim'
   pem_file = File.expand_path(File.join(File.dirname(__FILE__), '..', 'dkim.pem'))
-  Dkim::private_key = open(pemv_file).read
+  Dkim::private_key = open(pem_file).read
 
   # This will sign all ActionMailer deliveries
   ActionMailer::Base.register_interceptor(Dkim::Interceptor)
