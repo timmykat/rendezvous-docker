26391e949aeec6e54d72cdf811d3cc856015602a
timmykat <tkinnel+github@gmail.com>
2021-06-23 22:40:23 -0400

    Update initializer

--- config/initializers/braintree.rb
+++ config/initializers/braintree.rb
@@ -1,12 +1,12 @@
 # config/initializers/braintree.rb
-# if Rails.env.production?
-#   Braintree::Configuration.environment  = :production
-# else
-#   Braintree::Configuration.environment  = :sandbox
-# end
-# Braintree::Configuration.merchant_id  = ENV['RENDEZVOUS_BRAINTREE_MERCHANT_ID']
-# Braintree::Configuration.public_key   = ENV['RENDEZVOUS_BRAINTREE_PUBLIC_KEY']
-# Braintree::Configuration.private_key  = ENV['RENDEZVOUS_BRAINTREE_PRIVATE_KEY']
+if Rails.env.production?
+  Braintree::Configuration.environment  = :production
+else
+  Braintree::Configuration.environment  = :sandbox
+end
+Braintree::Configuration.merchant_id  = ENV['BRAINTREE_MERCHANT_ID']
+Braintree::Configuration.public_key   = ENV['BRAINTREE_PUBLIC_KEY']
+Braintree::Configuration.private_key  = ENV['BRAINTREE_PRIVATE_KEY']
 
 if !ENV["BRAINTREE_PAYMENT_TOKEN"]
   raise "Cannot find necessary environmental variables. See https://github.com/braintree/braintree_rails_example#setup-instructions for instructions";
