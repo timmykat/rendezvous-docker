dbaecf147d96a0ed45ee8dcae02e0eabbe0f52e8
timmykat <tkinnel+github@gmail.com>
2022-03-26 12:24:39 -0400

    Add delete wording

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -1,56 +1,58 @@
 $(function() {
-  $('#tabbed').tabs();
+    $('#tabbed').tabs();
 
-  $('#user-table').tablesorter();
+    $('#user-table').tablesorter();
 
-  $('.admin_check').change(function(e) {
-    let checked = false;
-    let $user = $(this).parent().parent();
-    let userId = $(this).attr('id').replace('is_admin_user_', '');
-    let sendData = { user_id: userId }
-    if ($(this).is(":checked")) {
-      sendData.admin = 'admin';
-      checked = true;
-    } else {
-      sendData.admin = false;
-    }
-    $.get('/ajax/toggle_admin', sendData, function() {
-      if (checked) {
-        $user.addClass('bg-success');
-      } else {
-        $user.removeClass('bg-success');
-      }
+    $('.admin_check').change(function(e) {
+        let checked = false;
+        let $user = $(this).parent().parent();
+        let userId = $(this).attr('id').replace('is_admin_user_', '');
+        let sendData = { user_id: userId }
+        if ($(this).is(":checked")) {
+            sendData.admin = 'admin';
+            checked = true;
+        } else {
+            sendData.admin = false;
+        }
+        $.get('/ajax/toggle_admin', sendData, function() {
+            if (checked) {
+                $user.addClass('bg-success');
+            } else {
+                $user.removeClass('bg-success');
+            }
+        });
     });
-  });
 
-  $('.tester_check').change(function(e) {
-    let checked = false;
-    let $user = $(this).parent().parent();
-    let userId = $(this).attr('id').replace('is_tester_user_', '');
-    let sendData = { user_id: userId }
-    if ($(this).is(":checked")) {
-      sendData.tester = 'tester';
-      checked = true;
-    } else {
-      sendData.tester = false;
-    }
-    $.get('/ajax/toggle_tester', sendData, function() {
-      if (checked) {
-        $user.addClass('bg-info');
-      } else {
-        $user.removeClass('bg-info');
-      }
+    $('.tester_check').change(function(e) {
+        let checked = false;
+        let $user = $(this).parent().parent();
+        let userId = $(this).attr('id').replace('is_tester_user_', '');
+        let sendData = { user_id: userId }
+        if ($(this).is(":checked")) {
+            sendData.tester = 'tester';
+            checked = true;
+        } else {
+            sendData.tester = false;
+        }
+        $.get('/ajax/toggle_tester', sendData, function() {
+            if (checked) {
+                $user.addClass('bg-info');
+            } else {
+                $user.removeClass('bg-info');
+            }
+        });
     });
-  });
 
-  $('button.delete_users').on('click', function() {
-    let $checkedUsers = $('.delete_user:checked');
-    let forDeletion = $.map($checkedUsers, function($check, index) {
-      return $check.defaultValue;
-    }).join(',');
+    $('button.delete_users').on('click', function() {
+        console.log("Clicked delete")
+        let $checkedUsers = $('.delete_user:checked');
+        let forDeletion = $.map($checkedUsers, function($check, index) {
+            return $check.defaultValue;
+        }).join(',');
 
-    $.get('/ajax/delete_users', {"users": forDeletion}, function() {
-      console.log('Success');
+        console.log(forDeletion);
+        $.get('/ajax/delete_users', { "users": forDeletion }, function() {
+            console.log('Success');
+        });
     });
-  });
 });
