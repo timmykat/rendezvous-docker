09d3b9a32e3fb952ccab45b8ae6a6ec11b1f8b64
timmykat <tkinnel+github@gmail.com>
2022-06-10 10:24:40 -0400

    Add registration close date

--- app/helpers/main_pages_helper.rb
+++ app/helpers/main_pages_helper.rb
@@ -1,5 +1,20 @@
 module MainPagesHelper
+  DAYS_OF_THE_WEEK = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ]
+
   def can_register?
     (current_user && (current_user.has_any_role? :admin, :tester)) || (Time.now > Rails.configuration.rendezvous[:registration_window][:open] && Time.now <= Rails.configuration.rendezvous[:registration_window][:close])
+  end
+
+  def formatted_date(the_date)
+    d_of_w = DAYS_OF_THE_WEEK[the_date.wday]
+    return "#{d_of_w}, #{the_date.strftime("%B %e")}"
   end
 end
