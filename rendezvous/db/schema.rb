da784b6887c9411dafca5b8a187c766f9e6b44ca
Tim Kinnel <tim_kinnel@wgbh.org>
2023-03-13 23:25:37 -0400

    Add bin files for handling delayed_job start; clean up some files; update rendezvous dates for production; Remove rendezvous/tmp files

--- rendezvous/db/schema.rb
+++ rendezvous/db/schema.rb
@@ -2,70 +2,69 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_11_064101) do
-
-  create_table "attendees", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
+ActiveRecord::Schema[7.0].define(version: 2023_03_11_064101) do
+  create_table "attendees", id: :integer, charset: "latin1", force: :cascade do |t|
     t.string "name"
     t.string "attendee_age", default: "adult"
     t.boolean "volunteer"
     t.boolean "sunday_dinner"
     t.integer "registration_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["attendee_age"], name: "index_attendees_on_attendee_age"
     t.index ["sunday_dinner"], name: "index_attendees_on_sunday_dinner"
     t.index ["volunteer"], name: "index_attendees_on_volunteer"
   end
 
-  create_table "delayed_jobs", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
+  create_table "delayed_jobs", id: :integer, charset: "latin1", force: :cascade do |t|
     t.integer "priority", default: 0, null: false
     t.integer "attempts", default: 0, null: false
     t.text "handler", null: false
     t.text "last_error"
-    t.datetime "run_at"
-    t.datetime "locked_at"
-    t.datetime "failed_at"
+    t.datetime "run_at", precision: nil
+    t.datetime "locked_at", precision: nil
+    t.datetime "failed_at", precision: nil
     t.string "locked_by"
     t.string "queue"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", precision: nil
+    t.datetime "updated_at", precision: nil
     t.index ["priority", "run_at"], name: "delayed_jobs_priority"
   end
 
-  create_table "main_pages", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table "main_pages", id: :integer, charset: "latin1", force: :cascade do |t|
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
   end
 
-  create_table "pictures", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
+  create_table "pictures", id: :integer, charset: "latin1", force: :cascade do |t|
     t.integer "user_id"
     t.string "image"
     t.string "caption"
     t.string "credit"
     t.integer "year"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
   end
 
-  create_table "registrations", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
+  create_table "registrations", id: :integer, charset: "latin1", force: :cascade do |t|
     t.integer "number_of_adults"
     t.integer "number_of_children"
     t.decimal "registration_fee", precision: 6, scale: 2
     t.text "events"
     t.integer "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", precision: nil
+    t.datetime "updated_at", precision: nil
     t.decimal "paid_amount", precision: 6, scale: 2
     t.string "paid_method"
-    t.datetime "paid_date"
+    t.datetime "paid_date", precision: nil
     t.string "year"
     t.string "invoice_number"
     t.decimal "donation", precision: 6, scale: 2
@@ -82,27 +81,27 @@ ActiveRecord::Schema.define(version: 2023_03_11_064101) do
     t.index ["year"], name: "index_registrations_on_year"
   end
 
-  create_table "transactions", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
+  create_table "transactions", id: :integer, charset: "latin1", force: :cascade do |t|
     t.string "transaction_method"
     t.string "transaction_type"
     t.string "cc_transaction_id"
     t.decimal "amount", precision: 6, scale: 2, default: "0.0"
     t.integer "registration_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", precision: nil
+    t.datetime "updated_at", precision: nil
     t.index ["cc_transaction_id"], name: "index_transactions_on_cc_transaction_id"
     t.index ["transaction_method"], name: "index_transactions_on_transaction_method"
     t.index ["transaction_type"], name: "index_transactions_on_transaction_type"
   end
 
-  create_table "users", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
+  create_table "users", id: :integer, charset: "latin1", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "reset_password_sent_at", precision: nil
+    t.datetime "remember_created_at", precision: nil
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.string "provider"
     t.string "uid"
     t.integer "role_mask"
@@ -130,7 +129,7 @@ ActiveRecord::Schema.define(version: 2023_03_11_064101) do
     t.index ["uid"], name: "index_users_on_uid"
   end
 
-  create_table "vehicles", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
+  create_table "vehicles", id: :integer, charset: "latin1", force: :cascade do |t|
     t.string "year"
     t.string "marque"
     t.string "model"
