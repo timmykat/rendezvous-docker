b669dfb561526372b266d5cd6f27c23ef0e38517
Tim Kinnel <tim_kinnel@wgbh.org>
2023-03-19 00:26:43 -0400

    Move csv files; add working nginx conf

--- rendezvous/config/deploy.rb
+++ rendezvous/config/deploy.rb
@@ -37,7 +37,7 @@ set :linked_files, fetch(:linked_files, []).push(
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push(
   'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 
-  'public/uploads', 'public/registrations', 'public/maintenance'
+  'public/uploads', 'public/registrations', 'public/maintenance', 'public/csv'
 )
 
 # Default value for default_env is {}
