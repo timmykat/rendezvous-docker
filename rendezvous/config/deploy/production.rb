1dd3d34ec2b1fdd368cb0be4bb64bf67eff27e3d
Tim Kinnel <tim_kinnel@wgbh.org>
2023-04-03 08:02:25 -0400

    Set can_register to false

--- rendezvous/config/deploy/production.rb
+++ rendezvous/config/deploy/production.rb
@@ -9,7 +9,8 @@
 
 server 'ec2-34-206-35-204.compute-1.amazonaws.com', user: 'ec2-user', roles: %w{app db web}
 set :rails_env, 'production'
-set :repo_url, 'ssh://ec2-34-206-35-204.compute-1.amazonaws.com/home/ec2-user/git-repos/rendezvous-docker.git'
+set :repo_url, 'git@github.com:timmykat/rendezvous-docker.git'
+# set :repo_url, 'ssh://ec2-34-206-35-204.compute-1.amazonaws.com/home/ec2-user/git-repos/rendezvous-docker.git'
 
 # set :repo_url, 'ssh://ec2-34-206-35-204.compute-1.amazonaws.com/home/ec2-user/git-repos/rendezvous.git'
 
