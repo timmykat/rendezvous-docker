7cede6ccda7eb5c3dde6f5b9451e0d00d14d8e3a
Tim Kinnel <tim_kinnel@wgbh.org>
2023-04-09 22:41:31 -0400

    Mostly working

--- rendezvous/app/assets/javascripts/application.js
+++ rendezvous/app/assets/javascripts/application.js
@@ -10,20 +10,28 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery/dist/jquery
-//= require jquery-ui/dist/jquery-ui
-//= require jquery-ui/ui/tabbable
-//= require jquery-ujs/src/rails
-//= require js-cookie/src/js.cookie
+
+//= require jquery
+//= require rails-ujs
+//= require html5shiv/dist/html5shiv.min
+//= require respond/main.js
 //= require bootstrap/dist/js/bootstrap
-//= require dropzone/dist/dropzone
-//= require cocoon
-//= require magnific-popup/dist/jquery.magnific-popup
+//= require jquery-ui
+//= require js-cookie/dist/js.cookie.min
+//= require tablesorter/dist/js/jquery.tablesorter
+//= require dropzone/dist/dropzone-min
+//= require magnific-popup/dist/jquery.magnific-popup.min
+//= require lazyload/lazyload.min
+//= require braintree-web/client
+//= require braintree-web/hosted-fields
+//= require @nathanvda/cocoon/cocoon
 //= require admin
+//= require payments
 //= require main_pages
+//= require custom
 //= require pictures
 //= require registrations
 //= require user
 //= require vehicles
-//= require tablesorter/dist/js/jquery.tablesorter
-//= require transactions
+
+
