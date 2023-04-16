0625bf6259f348c58487d17d239b04871ad9a0cf
Tim Kinnel <tim_kinnel@wgbh.org>
2023-04-16 16:46:55 -0400

    Clean up a bunch of stuff and put all the jquery stuff in closers

--- rendezvous/app/assets/javascripts/pictures.js
+++ rendezvous/app/assets/javascripts/pictures.js
@@ -11,58 +11,60 @@ Dropzone.options.dropzoneUpload = {
       this.removeFile(file);
     });
   }
-}
+};
 
-$(function() {
-  // Picture actions
-  $('table.pictures').on('click', 'button.delete', function(e) {
-    e.preventDefault();
-    var $button = $(e.target);
-    if ($button.hasClass('delete')) {
-      var id = $button.attr('id').replace('delete_','');
-      $.get('/ajax/picture/delete/' + id, function() {
-        $button.parent().parent().remove();
-      });
-    }
-  });
-  $('table.pictures').on('blur', '.credit input', function(e) {
-    var $row = $(e.target).closest('tr');
-    var picId = $row.attr('class').split('-')[1]
-    $.ajax({
-      url: '/pictures/' + picId,
-      method: 'put',
-      data: { 'picture[credit]': $(e.target).val() },
-      dataType: 'json',
-      success: function() {
-        $row.css({ 'backgroundColor': '#CFC' });
-        $row.animate({ 'backgroundColor': 'none' }, 1000);
-      }
-    }); 
-    return false;  
-  });
-  $('table.pictures').on('change', '.year select', function(e) {
-    var $row = $(e.target).closest('tr');
-    var picId = $row.attr('class').split('-')[1]
-    $.ajax({
-      url: '/pictures/' + picId,
-      method: 'put',
-      data: { 'picture[year]': $(e.target).val() },
-      dataType: 'json',
-      success: function() {
-        $row.css({ 'backgroundColor': '#CFC' });
-        $row.animate({ 'backgroundColor': 'none' }, 1000);
+(function($) {
+  jQuery(document).ready(function() {
+    // Picture actions
+    $('table.pictures').on('click', 'button.delete', function(e) {
+      e.preventDefault();
+      var $button = $(e.target);
+      if ($button.hasClass('delete')) {
+        var id = $button.attr('id').replace('delete_','');
+        $.get('/ajax/picture/delete/' + id, function() {
+          $button.parent().parent().remove();
+        });
       }
     });
-    return false;   
+    $('table.pictures').on('blur', '.credit input', function(e) {
+      var $row = $(e.target).closest('tr');
+      var picId = $row.attr('class').split('-')[1]
+      $.ajax({
+        url: '/pictures/' + picId,
+        method: 'put',
+        data: { 'picture[credit]': $(e.target).val() },
+        dataType: 'json',
+        success: function() {
+          $row.css({ 'backgroundColor': '#CFC' });
+          $row.animate({ 'backgroundColor': 'none' }, 1000);
+        }
+      }); 
+      return false;  
+    });
+    $('table.pictures').on('change', '.year select', function(e) {
+      var $row = $(e.target).closest('tr');
+      var picId = $row.attr('class').split('-')[1]
+      $.ajax({
+        url: '/pictures/' + picId,
+        method: 'put',
+        data: { 'picture[year]': $(e.target).val() },
+        dataType: 'json',
+        success: function() {
+          $row.css({ 'backgroundColor': '#CFC' });
+          $row.animate({ 'backgroundColor': 'none' }, 1000);
+        }
+      });
+      return false;   
+    });
+    
+    $('#gallery').magnificPopup({
+      delegate: 'a', // child items selector, by clicking on it popup will open
+      type: 'image',
+      gallery: { 
+        enabled: true,
+        navigateByImgClick: true, 
+        preload: [0,1]
+      }
+    });
   });
-  
-  $('#gallery').magnificPopup({
-    delegate: 'a', // child items selector, by clicking on it popup will open
-    type: 'image',
-    gallery: { 
-      enabled: true,
-      navigateByImgClick: true, 
-      preload: [0,1]
-    }
-  });
-});
+}) (jQuery);
--- rendezvous/app/assets/javascripts/registrations.js
+++ rendezvous/app/assets/javascripts/registrations.js
@@ -11,116 +11,113 @@
 //    return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
 // };
 
-$(function() {
-  var setRegistrationFee = function() {
-    if (typeof appData != 'undefined') {
-      var total = $('input#event_registration_number_of_adults').val() * appData.fees.adult
-         + $('input#event_registration_number_of_adults').val() * appData.fees.senior
-         + $('input#event_registration_number_of_children').val() * appData.fees.child;
-      $('input#event_registration_registration_fee').val((total).toFixed(2));
-      console.log('Registration fee:' + total)
+(function($) {
+  jQuery(document).ready(function() {
+    var setRegistrationFee = function() {
+      if (typeof appData != 'undefined') {
+        var total = $('input#event_registration_number_of_adults').val() * appData.fees.adult
+          + $('input#event_registration_number_of_adults').val() * appData.fees.senior
+          + $('input#event_registration_number_of_children').val() * appData.fees.child;
+        $('input#event_registration_registration_fee').val((total).toFixed(2));
+      }
+    };
+    var getAttendeeTotals = function() {
+      var adults = $('#attendees input[value="adult"]:visible:checked').length;
+      $('input#event_registration_number_of_adults').val(adults);
+      var seniors = $('#attendees input[value="senior"]:visible:checked').length;
+      $('input#event_registration_number_of_seniors').val(seniors);  
+      var children = $('#attendees input[value="child"]:visible:checked').length;
+      $('input#event_registration_number_of_children').val(children);  
+      setRegistrationFee();  
     }
-  };
-  var getAttendeeTotals = function() {
-    var adults = $('#attendees input[value="adult"]:visible:checked').length;
-    $('input#event_registration_number_of_adults').val(adults);
-    var seniors = $('#attendees input[value="senior"]:visible:checked').length;
-    $('input#event_registration_number_of_seniors').val(seniors);  
-    var children = $('#attendees input[value="child"]:visible:checked').length;
-    $('input#event_registration_number_of_children').val(children);  
-    setRegistrationFee();  
-  }
-      
-  // Remove the remove button from the first attendee and lock and hide the child selection
-  $('#attendees .remove_association_action:first').remove();
-  $('#attendees input[value="child"]:first').attr('disabled', true);
-  $('#attendees input[value="child"]:first').parent().hide();
-  $('#attendees .event_registration_attendees_name input:first').attr('placeholder', 'Your name *');
-  
-  // Initialize - set the first attendee info and get totals
-  // $('#attendees input[value="adult"]:first').attr('checked', 'checked');
-  getAttendeeTotals(); 
-  
-  if ($('input#event_registration_user_attributes_last_name').length > 0) {
-    var firstName = $('input#event_registration_user_attributes_first_name').val();
-    var lastName = $('input#event_registration_user_attributes_last_name').val();
-    $('#attendees input[placeholder="Your name *"]:first').val(firstName + ' ' + lastName);
-  }
-  
-
-  // Get adult, senior, and kid totals
-  $('#attendees').on('click', 'input[type=radio]', function(e) { 
+        
+    // Remove the remove button from the first attendee and lock and hide the child selection
+    $('#attendees .remove_association_action:first').remove();
+    $('#attendees input[value="child"]:first').attr('disabled', true);
+    $('#attendees input[value="child"]:first').parent().hide();
+    $('#attendees .event_registration_attendees_name input:first').attr('placeholder', 'Your name *');
+    
+    // Initialize - set the first attendee info and get totals
+    // $('#attendees input[value="adult"]:first').attr('checked', 'checked');
     getAttendeeTotals(); 
-  });
-  $('#attendees').on('cocoon:after-insert cocoon:after-remove', function(e) {
-    getAttendeeTotals();
-  });
-  
+    
+    if ($('input#event_registration_user_attributes_last_name').length > 0) {
+      var firstName = $('input#event_registration_user_attributes_first_name').val();
+      var lastName = $('input#event_registration_user_attributes_last_name').val();
+      $('#attendees input[placeholder="Your name *"]:first').val(firstName + ' ' + lastName);
+    }
+    
 
-  // Update registration fee
-  $('.fee-calculation').on('change click keyup', function(e) {
-    setRegistrationFee();  
-  });
-  
-  // Get final total on payment page
-  var setTotal = function() {
-    if (typeof appData != 'undefined') {
-      var donation = $('input[name="event_registration[donation]"]:checked').val();
-      if (donation == 'other') {
-        donation = $('input[name="event_registration[donation]"][type=number]').val();
+    // Get adult, senior, and kid totals
+    $('#attendees').on('click', 'input[type=radio]', function(e) { 
+      getAttendeeTotals(); 
+    });
+    $('#attendees').on('cocoon:after-insert cocoon:after-remove', function(e) {
+      getAttendeeTotals();
+    });
+    
+
+    // Update registration fee
+    $('.fee-calculation').on('change click keyup', function(e) {
+      setRegistrationFee();  
+    });
+    
+    // Get final total on payment page
+    var setTotal = function() {
+      if (typeof appData != 'undefined') {
+        var donation = $('input[name="event_registration[donation]"]:checked').val();
+        if (donation == 'other') {
+          donation = $('input[name="event_registration[donation]"][type=number]').val();
+        }
+
+        if ($.isNumeric(donation)) {
+          donation = parseFloat(donation);
+        } else {
+          donation = 0.
+        }
+        var total = parseFloat(appData.event_registration_fee) + donation;
+        $('input#event_registration_total').val(total.toFixed(2));
       }
+    };
+    
+    // Update total
+    $('.total-calculation').on('click blur', function(e) {
+      setTotal();
+    });
 
-      if ($.isNumeric(donation)) {
-        donation = parseFloat(donation);
+    // Toggle access to donation other amount field
+    $('input[type=radio].total-calculation').on('click', function(e) {
+      if ($(this).val() == 'other') {
+        $('input#event_registration_donation').attr('disabled', false);
       } else {
-        donation = 0.
+        $('input#event_registration_donation').attr('disabled', true).val('');
       }
-      console.log('Donation value: ', donation)
-      var total = parseFloat(appData.event_registration_fee) + donation;
-      console.log('Total: ', total)
-      console.log($('input#event_registration_total'))
-      $('input#event_registration_total').val(total.toFixed(2));
-      console.log('Final payment total is: ' + total)
-    }
-  };
-  
-  // Update total
-  $('.total-calculation').on('click blur', function(e) {
-    setTotal();
-  });
+    });
+    
+    
+    // Enable the email, amount and adult-, senior-, and child-count fields upon form submission
+    $('form').bind('submit', function() {
+      $('input.calculated, input[type=email]').prop('disabled', false);
+    });
+    
+    // Toggle CC vs mailing address
+    $('input.payment').on('click', function() {
+      $('#payment-form').toggleClass('hidden');
+      $('#mailing-address').toggleClass('hidden');
+    });  
+    
+    // Transactions
+    $('#transaction_transaction_method').on('change', function(e) {
+      if (e.target.value == 'credit card') {
+        $('#transaction_cc_transaction_id').attr('disabled', false);
+      } else {
+        $('#transaction_cc_transaction_id').attr('disabled', true);
+      }
+    });
 
-  // Toggle access to donation other amount field
-  $('input[type=radio].total-calculation').on('click', function(e) {
-    if ($(this).val() == 'other') {
-      $('input#event_registration_donation').attr('disabled', false);
-    } else {
-      $('input#event_registration_donation').attr('disabled', true).val('');
-    }
+    // Spinner
+    $('.go-to-payment').click(function(e) {
+      $('.review-loader').show();
+    })
   });
-  
-  
-  // Enable the email, amount and adult-, senior-, and child-count fields upon form submission
-  $('form').bind('submit', function() {
-    $('input.calculated, input[type=email]').prop('disabled', false);
-  });
-  
-  // Toggle CC vs mailing address
-  $('input.payment').on('click', function() {
-    $('#payment-form').toggleClass('hidden');
-    $('#mailing-address').toggleClass('hidden');
-  });  
-  
-  // Transactions
-  $('#transaction_transaction_method').on('change', function(e) {
-    if (e.target.value == 'credit card') {
-      $('#transaction_cc_transaction_id').attr('disabled', false);
-    } else {
-       $('#transaction_cc_transaction_id').attr('disabled', true);
-    }
-  });
-
-  // Spinner
-  $('.go-to-payment').click(function(e) {
-    $('.review-loader').show();
-  })
-});
+}) (jQuery);
