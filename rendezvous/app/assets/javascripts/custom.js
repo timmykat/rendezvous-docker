5796fb8fadc0ff8e7b14fc4b034bd2d4bb52e972
Tim Kinnel <tim_kinnel@wgbh.org>
2024-02-19 13:45:52 -0500

    Updates for functionality and hotel info

--- rendezvous/app/assets/javascripts/custom.js
+++ rendezvous/app/assets/javascripts/custom.js
@@ -1,71 +1,31 @@
-(function($) {
-    jQuery(document).ready(function() {
-    
-    //lightbox
-    // $(document).ready(function(){
-    //   $('a').nivoLightbox();
-    // });
-        //this code for the croll animation
-    $(document).ready(function () {
-        $(document).on("scroll", onScroll);
-        
-        //smoothscroll
-        $('a[href^="/#"]').on('click', function (e) {
-            $(document).off("scroll");
-            
-            $('a').each(function () {
-                $(this).removeClass('active');
-            })
-            $(this).addClass('active');
-        
-            var target = this.hash,
-                menu = target;
-            $target = $(target);
-            
-            
-            if (typeof($target.offset()) != 'undefined') {
+window.onload = (e) => {
+    window.addEventListener('scroll', e => {
+        let header = document.querySelector('header.top-header')
+        let stickyPoint = 115;
+        let position = window.scrollY
+
+        if (position > stickyPoint) {
+            header.classList.add('small')
+        } else {
+            header.classList.remove('small')
+        }
+    })
+
+    function scrollToElement(el) {
+        if (el) {
+            const yOffset = -50; // Adjust this value to account for any fixed headers, etc.
+            const y = el.getBoundingClientRect().top + window.scrollY + yOffset;
+            window.scrollTo({ top: y, behavior: 'smooth' });
+        }
+    }
+
+    let scrollLinks = document.querySelectorAll('header.top-header a[href*="#"]');
+    scrollLinks.forEach(link => {
+        link.addEventListener('click', e => {
             e.preventDefault();
-            $('html, body').stop().animate({
-                'scrollTop': $target.offset().top+2
-            }, 500, 'swing', function () {
-                window.location.hash = target;
-                $(document).on("scroll", onScroll);
-            });
-            } 
-        });
-
-        //this code is for header
-        $(window).scroll(function() {
-            var windowScrollPosTop = jQuery(window).scrollTop();
-
-            if(windowScrollPosTop >= 200) {
-                jQuery(".logo").css({"margin-top": "20px", "margin-bottom": "20px"});
-                jQuery(".navbar-default").css({"margin-top": "7px", "margin-bottom": "0"});
-            }
-            else{
-                jQuery(".logo").css({"margin-top": "8px", "border-bottom": "0"});
-                jQuery(".navbar-default").css({"margin-top": "10px", "margin-bottom": "5px"});
-                
-            }
-        });
+            let targetId = e.target.getAttribute('href').replace('/#', '');
+            let targetElement = document.getElementById(targetId);
+            scrollToElement(targetElement)
+        })
     });
-
-    function onScroll(event){
-        var scrollPos = $(document).scrollTop();
-        $('#menu-center a').each(function () {
-            var currLink = $(this);
-            var refElement = $(currLink.attr("href"));
-            if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
-                $('#menu-center ul li a').removeClass("active");
-                currLink.addClass("active");
-            }
-            else{
-                currLink.removeClass("active");
-            }
-        });
-    };
-
-
-    });
-
-}) (jQuery);
+}
