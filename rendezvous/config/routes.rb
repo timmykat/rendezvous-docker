6ba8d0b66318809299f235d76a0a860142abc6ac
Tim Kinnel <tim_kinnel@wgbh.org>
2024-04-13 15:06:03 -0400

    WIP

--- rendezvous/config/routes.rb
+++ rendezvous/config/routes.rb
@@ -52,6 +52,7 @@ Rails.application.routes.draw do
   namespace :admin do
     namespace :event do
       resources  :registrations, { only: [ :show, :edit, :update ] }
+      get 'registrations/new/user/:id', to: 'registrations#new_with_user'
       get 'registrations/:id/cancel', to: 'registrations#cancel', as: 'cancel_registration'
       get 'registrations/:id/send_confirmation_email', to: 'registrations#send_confirmation_email'
     end
