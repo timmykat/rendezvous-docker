1664152c7f061ae511ce0e44114e4450f920e1a2
Tim Kinnel <tim_kinnel@wgbh.org>
2024-04-21 07:59:02 -0400

    User clean up good

--- rendezvous/app/controllers/admin/users_controller.rb
+++ rendezvous/app/controllers/admin/users_controller.rb
@@ -1,24 +1,19 @@
 class Admin::UsersController < AdminController
   def cleanup
-    @users = get_users
-  end
-
-  def delete_users
-    Rails.logger.debug("****")
-    Rails.logger.debug(params.inspect)
+    @users = get_deletion_candidates
     @users_to_delete = User.where(id: params[:user_ids])
-    @users = get_users
-    redirect_to admin_cleanup_path
+    confirm_delete = params[:confirm_delete]
+    if confirm_delete && !@users_to_delete.empty?
+      @users_to_delete.destroy_all
+      redirect_to admin_index_path, notice: "Sus users deleted"
+    end
   end
 
-  def get_users
-    users = User.left_outer_joins(:registrations).where(registrations: { id: nil })
-    users = @users.where("CAST(last_name AS BINARY) REGEXP BINARY ?", "[A-Z]{2}").all
+  def get_deletion_candidates
+    users = User.left_outer_joins(:registrations).where(registrations: { id: nil }).all
+    # users = @users.where("CAST(last_name AS BINARY) REGEXP BINARY ?", "[A-Z]{2}")
+    # users = @users.where("CAST(last_name AS BINARY) REGEXP BINARY ?", "^(?!([A-Z]+))$")
     # @users = @users.where("BINARY last_name REGEXP ?", "^(?!(Mc|Mac|La|De))")
     # @users = @users.where("last_name REGEXP ?", "^(?![A-Z]+$)")
-  end
-
-  def users_to_delete_params
-    params.require(:user_ids)
   end
 end
