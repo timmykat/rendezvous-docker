257ca10e9c3a954ca1211daf72651ad0cccd4dd1
Tim Kinnel <tim_kinnel@wgbh.org>
2024-04-20 21:38:24 -0400

    WIP

--- rendezvous/app/controllers/admin/users_controller.rb
+++ rendezvous/app/controllers/admin/users_controller.rb
@@ -1,15 +1,24 @@
 class Admin::UsersController < AdminController
   def cleanup
-    @users = User.joins(:registrations)
-    .group('users.id')
-    .having('COUNT(registrations.id) = 0')
-    # @users = @users.where("CAST(last_name AS BINARY) REGEXP BINARY ?", "[A-Z]{2}").all
+    @users = get_users
+  end
+
+  def delete_users
+    Rails.logger.debug("****")
+    Rails.logger.debug(params.inspect)
+    @users_to_delete = User.where(id: params[:user_ids])
+    @users = get_users
+    redirect_to admin_cleanup_path
+  end
+
+  def get_users
+    users = User.left_outer_joins(:registrations).where(registrations: { id: nil })
+    users = @users.where("CAST(last_name AS BINARY) REGEXP BINARY ?", "[A-Z]{2}").all
     # @users = @users.where("BINARY last_name REGEXP ?", "^(?!(Mc|Mac|La|De))")
     # @users = @users.where("last_name REGEXP ?", "^(?![A-Z]+$)")
   end
 
-  def delete_users
-    @users_to_delete = User.find(params[:user_id])
-    redirect_to admin_cleanup_path
+  def users_to_delete_params
+    params.require(:user_ids)
   end
 end
