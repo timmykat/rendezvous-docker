150bbfde6fa412dca96bad27a5826ebaa514a8f8
Tim Kinnel <tim_kinnel@wgbh.org>
2024-07-13 15:39:39 -0400

    Update puma config

--- rendezvous/config/puma.rb
+++ rendezvous/config/puma.rb
@@ -11,27 +11,27 @@ environment rails_env
 
 
 
-unless docker_env?
-  app_dir = "/var/www/rendezvous-docker/rendezvous"
-  shared_dir = "#{app_dir}/shared"
-  db_config_file = "#{shared_dir}/config/database.yml"
-  puma_socket_file = "#{shared_dir}/tmp/sockets/puma.sock"
-  puma_state_file = "#{shared_dir}/tmp/pids/puma.state"
-  pidfile "#{shared_dir}/tmp/pids/puma.pid"
-  log_dir = "#{shared_dir}/log"
+# unless docker_env?
+#   app_dir = "/var/www/rendezvous-docker/rendezvous"
+#   shared_dir = "#{app_dir}/shared"
+#   db_config_file = "#{shared_dir}/config/database.yml"
+#   puma_socket_file = "#{shared_dir}/tmp/sockets/puma.sock"
+#   puma_state_file = "#{shared_dir}/tmp/pids/puma.state"
+#   pidfile "#{shared_dir}/tmp/pids/puma.pid"
+#   log_dir = "#{shared_dir}/log"
 
-  workers 1
+#   workers 1
 
-else
-  app_dir = "/var/www/rendezvous"
-  db_config_file = "#{app_dir}/config/database.yml"
-  puma_socket_file = "#{app_dir}/tmp/sockets/puma.sock"
-  puma_state_file = "#{app_dir}/tmp/pids/puma.state"
-  pidfile "#{app_dir}/tmp/pids/puma.pid"
-  log_dir = "#{app_dir}/log"
+# else
+app_dir = "/var/www/rendezvous"
+db_config_file = "#{app_dir}/config/database.yml"
+puma_socket_file = "#{app_dir}/tmp/sockets/puma.sock"
+puma_state_file = "#{app_dir}/tmp/pids/puma.state"
+pidfile "#{app_dir}/tmp/pids/puma.pid"
+log_dir = "#{app_dir}/log"
 
-  puts "*** Puma socket: #{puma_socket_file}"
-end
+puts "*** Puma socket: #{puma_socket_file}"
+# end
 
 # Bind puma socket
 bind "unix:/#{puma_socket_file}"
