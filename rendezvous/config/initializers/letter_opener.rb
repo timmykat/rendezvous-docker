6a6a7cbd91ecf3f724f8a0e35fd1eea3cf7a99b8
Tim Kinnel <tim_kinnel@wgbh.org>
2024-07-16 21:56:18 -0400

    Basically working again lol

--- rendezvous/config/initializers/letter_opener.rb
+++ rendezvous/config/initializers/letter_opener.rb
@@ -1,19 +1,17 @@
 require 'letter_opener'
 
-if docker_env?
-  LetterOpener.configure do |config|
-    # To overrider the location for message storage.
-    # Default value is `tmp/letter_opener`
-    config.location = Rails.root.join('tmp', 'testing_mails')
+LetterOpener.configure do |config|
+  # To overrider the location for message storage.
+  # Default value is `tmp/letter_opener`
+  config.location = Rails.root.join('tmp', 'testing_mails')
 
-    # To render only the message body, without any metadata or extra containers or styling.
-    # Default value is `:default` that renders styled message with showing useful metadata.
-    # config.message_template = :light
+  # To render only the message body, without any metadata or extra containers or styling.
+  # Default value is `:default` that renders styled message with showing useful metadata.
+  # config.message_template = :light
 
-    # To change default file URI scheme you can provide `file_uri_scheme` config.
-    # It might be useful when you use WSL (Windows Subsystem for Linux) and default
-    # scheme doesn't work for you.
-    # Default value is blank
-    # config.file_uri_scheme = 'file://///wsl$/Ubuntu-18.04'
-  end
+  # To change default file URI scheme you can provide `file_uri_scheme` config.
+  # It might be useful when you use WSL (Windows Subsystem for Linux) and default
+  # scheme doesn't work for you.
+  # Default value is blank
+  # config.file_uri_scheme = 'file://///wsl$/Ubuntu-18.04'
 end
--- rendezvous/lib/docker/docker_helper.rb
+++
@@ -1,7 +0,0 @@
-module Rendezvous
-  module Docker
-    def docker_env?
-      Dir.exist? '/proc/docker'
-    end
-  end
-end
