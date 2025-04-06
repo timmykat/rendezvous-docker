40620cfb5bff66dc85016d68390951ebccd0b167
Tim Kinnel <tim_kinnel@wgbh.org>
2025-04-06 16:40:45 -0400

    fix migration

--- rendezvous/db/migrate/20250406173059_add_user_to_vendor.rb
+++ rendezvous/db/migrate/20250406173059_add_user_to_vendor.rb
@@ -1,9 +1,6 @@
 class AddUserToVendor < ActiveRecord::Migration[7.1]
   def change
-    remove_foreign_key :vendors, :users rescue nil
-
-    change_column :vendors, :user_id, :integer
-
+    add_column :vendors, :user_id, :integer
     add_foreign_key :vendors, :users
   end
 end
