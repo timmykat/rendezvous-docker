4899da040440932627391767fe46f0cf4b1270f3
Tim Kinnel <tim_kinnel@wgbh.org>
2025-04-11 22:59:55 -0400

    WIP

--- rendezvous/app/models/donation.rb
+++ rendezvous/app/models/donation.rb
@@ -1,2 +1,11 @@
 class Donation < ApplicationRecord
+  
+  belongs_to :user
+  belongs_to :registration
+
+  STATUSES = ['created', 'complete']
+
+  validates :status, inclusion: { in: STATUSES }
+
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }, presence: true
 end
--- rendezvous/db/migrate/20250411223902_create_donations.rb
+++ rendezvous/db/migrate/20250411223902_create_donations.rb
@@ -1,11 +1,13 @@
 class CreateDonations < ActiveRecord::Migration[7.1]
   def change
     create_table :donations do |t|
-      t.int :user_id, index: true
-      t.date :date, index: true
-      t.string :first_name
-      t.string :last_name
+      t.integer :user_id, index: true
+      t.integer :registration_id, index: true
+      t.date    :date, index: true
+      t.string  :first_name
+      t.string  :last_name
       t.decimal :amount, precision: 6, scale: 2
+      s.string  :status
 
       t.timestamps
     end
