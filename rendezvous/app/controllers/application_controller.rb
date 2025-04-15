da7551e73949dbbe9f8a3d3593e80947e3b7aae0
Tim Kinnel <tim_kinnel@wgbh.org>
2025-04-14 22:19:22 -0400

    Set login redirect using devise

--- rendezvous/app/controllers/application_controller.rb
+++ rendezvous/app/controllers/application_controller.rb
@@ -102,7 +102,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource)
-    user_path(resource)
+    stored_location_for(resource) || root_path
   end
 
   def after_sign_out_path_for(resource_or_scope)
