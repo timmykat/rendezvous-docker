5477692431a99327813fc2d7750c547ea773c2d8
Tim Kinnel <tim_kinnel@wgbh.org>
2025-04-17 00:12:37 -0400

    Remove headers

--- rendezvous/app/controllers/main_pages_controller.rb
+++ rendezvous/app/controllers/main_pages_controller.rb
@@ -78,7 +78,9 @@ class MainPagesController < ApplicationController
   end
 
   def set_cache_headers
-    response.headers['Cache-Control'] = 'public, max-age=86400'
+    response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = 0
   end
 
   private
