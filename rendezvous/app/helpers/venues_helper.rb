e0cd8e282a8286515352c836a3f86f95ab35f8d2
Tim Kinnel <tim_kinnel@wgbh.org>
2025-04-19 17:18:21 -0400

    Add venue info for full schedule

--- rendezvous/app/helpers/venues_helper.rb
+++ rendezvous/app/helpers/venues_helper.rb
@@ -4,11 +4,11 @@ module VenuesHelper
   end
 
   def format_for_schedule(venue)
-    "<strong>#{venue.name}, #{short_address(venue.address)}"
+    "<strong>#{venue.name}, #{short_address(venue.address)}</strong>"
   end
 
   def short_address address
-    address_lines = address.split("\n")
+    address_lines = address.gsub("\\", '').split("\n")
     "#{address_lines[0]}, #{address_lines[1].split(",")[0]}"
   end
 
