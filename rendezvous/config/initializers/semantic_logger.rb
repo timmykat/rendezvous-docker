87fd498a13e4249da9230ecf1add13b9d6105f7b
Tim Kinnel <kinnel@warpmail.net>
2026-03-29 23:42:43 -0400

    Trying to solve logging issues

--- rendezvous/config/initializers/semantic_logger.rb
+++ rendezvous/config/initializers/semantic_logger.rb
@@ -2,11 +2,6 @@ require 'semantic_logger'
 
 SemanticLogger.default_level = Rails.env.production? ? :info : :debug
 
-# File appender (persistent logs)
-SemanticLogger.add_appender(
-  file_name: Rails.root.join("log/#{Rails.env}.log").to_s,
-  formatter: :default
-)
 
 # STDOUT appender (Docker-friendly)
 SemanticLogger.add_appender(
--- rendezvous/config/puma.rb
+++ rendezvous/config/puma.rb
@@ -6,8 +6,6 @@ environment rails_env
 
 pidfile "#{app_dir}/tmp/pids/puma.pid"
 state_path "#{app_dir}/tmp/pids/puma.state"
-log_dir = "#{app_dir}/log"
-stdout_redirect "#{log_dir}/puma.stdout.log", "#{log_dir}/puma.stderr.log", true
 
 threads 1, 5
 
