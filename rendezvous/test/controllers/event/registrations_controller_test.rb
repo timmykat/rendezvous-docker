81d2d0eb7e4141303beb7002df73e276d29ada7c
Tim Kinnel <kinnel@warpmail.net>
2026-03-30 22:40:47 -0400

    WIP

--- rendezvous/test/controllers/event/registrations_controller_test.rb
+++ rendezvous/test/controllers/event/registrations_controller_test.rb
@@ -1,42 +1,30 @@
 # test/controllers/event/registrations_controller_test.rb
+require 'test_helper'
 
-require "test_helper"
+module Event
+  class RegistrationsControllerTest < ActionDispatch::IntegrationTest
 
-class Event::RegistrationsControllerTest < ActionDispatch::IntegrationTest
+    test 'sets paid amount to 0 when paying by check' do
+      registration = build(:registration, paid_method: 'cash or check')
 
-  test 'sets paid amount to 0 when paying by check' do
-    registration = build(:registration, paid_method: 'cash or check')
-
-    assert_equal 0.0, registration.paid_amount
-  end
-
-  test 'creates an invoice number' do
-    registration = build(:registration)
-    year = Date.current.year
-
-    Registration.stub :invoice_number, "CR#{year}-999" do
-      assert_equal "CR#{year}-999", registration.invoice_number
+      assert_equal 0.0, registration.paid_amount
     end
-  end
 
-  test "creates a user if not logged in and user doesn't exist" do
-    registration = create(:registration)
+    test 'creates an invoice number' do
+      registration = build(:registration)
+      year = Date.current.year
 
-    assert_equal User, registration.user.class
-  end
+      Registration.stub :invoice_number, "CR#{year}-999" do
+        assert_equal "CR#{year}-999", registration.invoice_number
+      end
+    end
 
-  test 'finds and updates existing user' do
-    user = create(:user)
-    registration = create(:registration)
+    test 'updates the user when logged in' do
+      user = create(:user)
 
-    assert_equal user, registration.user
-  end
+      registration = create(:registration, user_attributes: { first_name: 'Jacques' })
 
-  test 'updates the user when logged in' do
-    user = create(:user)
-
-    registration = create(:registration, user_attributes: { first_name: 'Jacques' })
-
-    assert_equal 'Jacques', user.reload.first_name
+      assert_equal 'Jacques', user.reload.first_name
+    end
   end
 end
