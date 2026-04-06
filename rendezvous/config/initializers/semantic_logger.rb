40ac1b03e35ebba0864f1e596853ad85b4015ba3
Tim Kinnel <kinnel@warpmail.net>
2026-04-06 18:47:47 -0400

    Single logger

--- rendezvous/config/initializers/semantic_logger.rb
+++ rendezvous/config/initializers/semantic_logger.rb
@@ -2,8 +2,12 @@ require 'semantic_logger'
 
 SemanticLogger.default_level = Rails.env.production? ? :info : :debug
 
+# Remove existing appenders
+SemanticLogger.appenders.each do |appender|
+  SemanticLogger.remove_appender(appender)
+end
 
-# STDOUT appender (Docker-friendly)
+# Add only STDOUT (Docker-friendly)
 SemanticLogger.add_appender(
   io: $stdout,
   formatter: :default
