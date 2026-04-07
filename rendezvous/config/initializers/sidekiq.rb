6a2f57659e67ba77fa11f49d950b4fd6153eff2c
Tim Kinnel <kinnel@warpmail.net>
2026-04-06 20:42:55 -0400

    Some more supposed fixes

--- rendezvous/config/initializers/sidekiq.rb
+++ rendezvous/config/initializers/sidekiq.rb
@@ -1,5 +1,5 @@
 # config/initializers/sidekiq.rb
-redis_url = ENV.fetch('REDIS_URL', 'redis://redis:6379/0')
+redis_url = ENV['REDIS_URL'] || 'redis://redis:6379/0'
 Sidekiq.configure_server do |config|
   config.redis = { url: redis_url }
 end
